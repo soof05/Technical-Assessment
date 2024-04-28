b5bc48e2cfeafcaa4487d791b02069c37e6bd9f8
sel-ouaf <113302382+soof05@users.noreply.github.com>
2024-04-28 15:58:19 +0000

    Switch to microservice and run it successfully

--- book-app/src/book.controller.ts
+++ book-app/src/book.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import {BookService } from './book.service';
 import { BookDto } from './dto/book-dto';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
-  @Get()
-  getAllBooks(): BookDto {
+  @MessagePattern('getBooks')
+  async getAllBooks(): Promise<BookDto> {
     return this.bookService.getBooks();
   }
 }
--- book-app/src/main.ts
+++ book-app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3001
+      }
+    }
+  )
+  await app.listen();
 }
 bootstrap();
